935f2cc71521ee7f8653f3438748d1c4a30530d4
ARDev097 <ardev029@gmail.com>
2024-09-04 17:55:17 +0530

    Removed dotted line for event end date

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -4,14 +4,14 @@ import { Chart as ChartJS, TimeScale, LinearScale, PointElement, LineElement, To
 import 'chartjs-adapter-date-fns';
 import annotationPlugin from 'chartjs-plugin-annotation';
 import { parse, format } from 'date-fns';
-import './LineGraph.css'; // Import the CSS file
+import './LineGraph.css'; 
 import oplogo from "./OP.png"
 
 ChartJS.register(TimeScale, LinearScale, PointElement, LineElement, Tooltip, Legend, annotationPlugin);
 
 const LineGraph = () => {
   const [chartData, setChartData] = useState(null);
-  const [view, setView] = useState('daily'); // 'daily' or 'movingAverage'
+  const [view, setView] = useState('daily'); 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,7 +29,7 @@ const LineGraph = () => {
     const cpiData = [];
 
     data.forEach(item => {
-      const date = parse(item.date, 'dd-MM-yyyy', new Date()); // Parse the date from 'dd-MM-yyyy' to Date object
+      const date = parse(item.date, 'dd-MM-yyyy', new Date()); 
       const cpi = parseFloat(item.CPI);
       if (date && !isNaN(cpi)) {
         labels.push(date);
@@ -56,59 +56,44 @@ const LineGraph = () => {
       },
     ];
 
-    // Define the events with their start and end dates
     const events = [
       { name: 'RPGF Round 2', startDate: '01-06-2022', endDate: '30-03-2023', color: 'rgba(255,0,0,0.7)' },
-      { name: 'RPGF Round 3', startDate: '14-10-2023', endDate: '11-01-2024', color: 'rgba(0,128,0,0.7)' },
-      { name: 'Season 3', startDate: '26-01-2023', endDate: '05-04-2023', color: 'rgba(255,165,0,0.7)' },
+      { name: 'RPGF Round 3', startDate: '14-10-2023', endDate: '11-01-2024', color: 'rgba(255,0,0,0.7)' },
+      { name: 'Season 3', startDate: '26-01-2023', endDate: '05-04-2023', color: 'rgba(128,0,128,0.7)' },
       { name: 'Season 4', startDate: '08-06-2023', endDate: '20-09-2023', color: 'rgba(128,0,128,0.7)' },
-      { name: 'Season 5', startDate: '04-01-2024', endDate: '08-05-2024', color: 'rgba(0,255,255,0.7)' },
+      { name: 'Season 5', startDate: '04-01-2024', endDate: '08-05-2024', color: 'rgba(128,0,128,0.7)' },
     ];
 
-    // Convert event dates to Date objects and create annotations for dotted lines and text labels
     const annotations = {};
 
     events.forEach((event, index) => {
       const start = parse(event.startDate, 'dd-MM-yyyy', new Date());
       const end = event.endDate ? parse(event.endDate, 'dd-MM-yyyy', new Date()) : labels[labels.length - 1];
       
-      // Add the start line with label
       annotations[`startLine${index}`] = {
         type: 'line',
         xMin: start,
         xMax: start,
         borderColor: event.color,
         borderWidth: 2,
-        borderDash: [6, 6], // Dotted line
-      };
-      
-      // Add the end line
-      annotations[`endLine${index}`] = {
-        type: 'line',
-        xMin: end,
-        xMax: end,
-        borderColor: event.color,
-        borderWidth: 2,
-        borderDash: [6, 6], // Dotted line
+        borderDash: [6, 6], 
       };
 
-      // Add text annotation for event name and duration
       annotations[`eventLabel${index}`] = {
         type: 'label',
-        xValue: (start.getTime() + end.getTime()) / 2,
+        xValue: start,
         yValue: Math.max(...cpiData),
-        content: `${event.name}`, // Updated format
+        content: `${event.name}`,
         font: {
-          size: 12,
+          size: 15,
           weight: 'bold',
         },
         color: event.color,
-        textAlign: 'center',
+        // textAlign: 'center',
         padding: {
-          // top: 1,
           bottom: 120,
-          left: 6,
-          right: 6,
+          left: 115,
+          // right: 6,
         },
       };
     });
@@ -144,13 +129,13 @@ const LineGraph = () => {
             options={{
               plugins: {
                 legend: {
-                  display: false,  // Disable the legend
+                  display: false,  
                 },
                 tooltip: {
                   callbacks: {
                     title: function (tooltipItem) {
                       const date = tooltipItem[0].label;
-                      return `Date - ${format(new Date(date), 'yyyy-MM-dd')}`; // Format YYYY-MM-DD
+                      return `Date - ${format(new Date(date), 'yyyy-MM-dd')}`; 
                     },
                     label: function (tooltipItem) {
                       const value = tooltipItem.raw;
